5beff03828a0e8bd9a3fc6a2ffd796506cbe0751
wyrustaaruz <wyrustaaruz@gmail.com>
2015-09-16 14:14:54 +0300

    oldu
    tamam

---
+++ Assets/Scripts/KutuDoluCikti.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KutuDoluCikti : MonoBehaviour {
+	public Renderer rend;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnMouseDown(){
+		
+		rend.material.color = Color.green;
+		
+	}
+}
